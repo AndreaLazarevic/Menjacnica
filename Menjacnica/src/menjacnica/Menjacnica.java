0b9b7dc7f30ad1185f417f9d7d65e6da978406e4
AndreaLazarevic <andrea.lazarevic1996@gmail.com>
2017-04-04 02:30:45 +0200

    Implementirane metode za dodavanje, brisanje i pretragu kursa u okviru klase Menjacnica.

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -1,27 +1,52 @@
 package menjacnica;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import interfejs_menjacnice.MenjacnicaInterfejs;
 
 public class Menjacnica implements MenjacnicaInterfejs {
+	LinkedList<Valuta> valute = new LinkedList<Valuta>();
 
 	@Override
 	public void dodajKursValuteNaDan(String oznakaValute, GregorianCalendar datum, double prodajniKurs, 
 			double srednjiKurs,	double kupovniKurs) {
-		// TODO Auto-generated method stub
-
+		for (int i = 0; i < valute.size(); i++) {
+			if(valute.get(i).getOznakaValute().equals(oznakaValute)){
+				Kurs k = new Kurs();
+				
+				k.setDatum(datum);
+				k.setProdajniKurs(prodajniKurs);
+				k.setSrednjiKurs(srednjiKurs);
+				k.setKupovniKurs(kupovniKurs);
+				
+				valute.get(i).getKursevi().add(k);
+			}
+				
+		}
 	}
 
 	@Override
 	public void obrisiKursValuteNaDan(String oznakaValute, GregorianCalendar datum) {
-		// TODO Auto-generated method stub
-
+		for (int i = 0; i < valute.size(); i++) {
+			if(valute.get(i).getOznakaValute().equals(oznakaValute) &&
+					valute.get(i).getKursevi().get(i).getDatum().equals(datum)){
+				
+				valute.get(i).getKursevi().remove(i);
+			}	
+		}
 	}
 
 	@Override
 	public double pronadjiIVratiKursValuteNaDan(String oznakaValute, GregorianCalendar datum) {
-		// TODO Auto-generated method stub
+		for (int i = 0; i < valute.size(); i++) {
+			if(valute.get(i).getOznakaValute().equals(oznakaValute) &&
+					valute.get(i).getKursevi().get(i).getDatum().equals(datum)){
+				
+				return valute.get(i).getKursevi().get(i).getSrednjiKurs();
+			}	
+		}
+		
 		return 0;
 	}
 
